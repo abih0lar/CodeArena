315e9882bbf25a3cf97c3a2990daf1a290ca56bd
Abiola Ogunbowale <bih0larOG@gmail.com>
2026-04-23 18:24:58 +0100

    Delete AUTOMATION_BOT_V3/start.sh

--- AUTOMATION_BOT_V3/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export MALLOC_ARENA_MAX=2
-echo "Starting Trading Bot..."
-python main.py &
-echo "Starting Streamlit Dashboard..."
-streamlit run dashboard.py --server.port $PORT --server.address 0.0.0.0 --browser.gatherUsageStats false --server.headless true
